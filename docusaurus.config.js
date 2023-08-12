02c23038cefc2dfddddf99c1d81edff414322114
Aniurm <534147085@qq.com>
2023-08-12 17:27:27 +0800

    feat: add config for Github Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,14 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cs-engineering.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'zjuxlab', // Usually your GitHub org/user name.
+  projectName: 'cs-engineering.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

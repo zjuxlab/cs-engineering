204c6e7815009ffa43be0fb0372611f0ade0a399
Aniurm <534147085@qq.com>
2023-10-28 15:32:28 +0800

    feat: cover

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'CS-Engineering',
+  tagline: 'ZJU XLab',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'CS-Engineering',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -129,7 +129,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ZJU XLab, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,

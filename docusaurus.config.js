7d32434719afcf147903bb2b9627204b4d163d17
Bbw486502970@outlook.com <Bbw486502970@outlook.com>
2025-03-10 20:18:01 +0800

    fix: remove global onBrokenLinks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,8 @@ const config = {
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  // onBrokenLinks: 'throw',
+  // onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
